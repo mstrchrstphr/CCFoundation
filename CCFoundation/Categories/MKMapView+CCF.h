f7483ea6d2d2f3e5a9511f02f0dce98d394a11c5
Christopher Constable <chris.m.constable@gmail.com>
2012-10-17 13:17:43 -0400

    Added new map view category.

---
+++ CCFoundation/Categories/MKMapView+CCF.h
@@ -0,0 +1,13 @@
+//
+//  MKMapView+CCF.h
+//  Traffic
+//
+//  Created by Christopher Constable on 10/11/12.
+//  Copyright (c) 2012 Futura IO. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MKMapView (CCF)
+
+@end
---
+++ CCFoundation/Categories/MKMapView+CCF.m
@@ -0,0 +1,13 @@
+//
+//  MKMapView+CCF.m
+//  Traffic
+//
+//  Created by Christopher Constable on 10/11/12.
+//  Copyright (c) 2012 Futura IO. All rights reserved.
+//
+
+#import "MKMapView+CCF.h"
+
+@implementation MKMapView (CCF)
+
+@end
