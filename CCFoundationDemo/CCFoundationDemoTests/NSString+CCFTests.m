245ee6a3c433db8db8859bd73945aa54d97380e3
Christopher Constable <chris.m.constable@gmail.com>
2014-02-05 20:01:28 -0500

    Add NSString+CFF_Web tests.

---
+++ CCFoundationDemo/CCFoundationDemoTests/NSString+CCFTests.m
@@ -0,0 +1,34 @@
+//
+//  NSString+CCFTests.m
+//  CCFoundationDemo
+//
+//  Created by Christopher Constable on 2/5/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSString_CCFTests : XCTestCase
+
+@end
+
+@implementation NSString_CCFTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
